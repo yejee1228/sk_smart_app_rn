7893767468058712e02f29e8fd4bad741c5c9027
yejee1228 <yejee1228@megastudy.net>
2022-03-08 09:47:14 +0900

    안드로이드 뒤로가기 기능

--- view/androidWebView.js
+++ view/androidWebView.js
@@ -1,99 +1,103 @@
 import React, { useRef, useState } from "react";
-/* import { BackHandler } from "react-native";
+import { BackHandler } from "react-native";
 import { useNavigation } from "@react-navigation/core";
-import { useFocusEffect } from "@react-navigation/native"; */
+import { useFocusEffect } from "@react-navigation/native";
 import { WebView } from "react-native-webview";
 
-/* const INJECTED_JAVASCRIPT = `(function() {
-  AsyncStorage.setItem("memberCode", 1);
-  AsyncStorage.setItem("isApp", true);
-})();`;
-
-const navigation = useNavigation();
-const webview = useRef(null);
-const [canGoBack, SetCanGoBack] = useState(false);
-
-// 안드로이드의 하드웨어적인 뒤로가기 설정
-useFocusEffect(
-    React.useCallback(() => {
-        const onBackPress = () => {
-            if (webview.current && canGoBack) {
-                webview.current.goBack();
-                return true;
-            } else {
-                return false;
-            }
-        };
-        BackHandler.addEventListener("hardwareBackPress", onBackPress);
-        return () =>
-            BackHandler.removeEventListener("hardwareBackPress", onBackPress);
-    }, [canGoBack])
-);
-
-// 안드로이드의 소프트웨어적인 뒤로가기 설정
-const backPress = (navState) => {
-    const { canGoBack } = navState;
-
-    if (canGoBack) {
-        navigation.setParams({
-            isCanBack: {
-                title: "",
-                onPress: () => webview.current.goBack(),
-            },
-        });
-    } else {
-        navigation.setParams({
-            isCanBack: null,
-        });
-    }
-};
- */
-
-const onShouldStartLoadWithRequest = (event) => {
-    if (
-        event.url.startsWith('http://') ||
-        event.url.startsWith('https://') ||
-        event.url.startsWith('about:blank')
-    ) {
-        return true;
-    }
-    if (Platform.OS === 'android') {
-        const SendIntentAndroid = require('react-native-send-intent');
-        SendIntentAndroid.openChromeIntent(event.url)
-            .then(isOpened => {
-                if (!isOpened) { alert('앱 실행이 실패했습니다'); }
-            })
-            .catch(err => {
-                console.log(err);
-            });
-
-        return false;
-
-    } else {
-        Linking.openURL(event.url)
-            .catch(err => {
-                alert('앱 실행이 실패했습니다. 설치가 되어있지 않은 경우 설치하기 버튼을 눌러주세요.');
-            });
-        return false;
-    }
-};
-
 const androidWebView = ({ url }) => {
+    const INJECTED_JAVASCRIPT = `(function() {
+        AsyncStorage.setItem("memberCode", 1);
+        AsyncStorage.setItem("isApp", true);
+        })();`;
+
+    const navigation = useNavigation();
+    const webview = useRef(null);
+    const [canGoBack, SetCanGoBack] = useState(false);
+
+    // 안드로이드의 하드웨어적인 뒤로가기 설정
+    useFocusEffect(
+        React.useCallback(() => {
+            const onBackPress = () => {
+                if (webview.current && canGoBack) {
+                    webview.current.goBack();
+                    return true;
+                } else {
+                    return false;
+                }
+            };
+            BackHandler.addEventListener("hardwareBackPress", onBackPress);
+            return () =>
+                BackHandler.removeEventListener("hardwareBackPress", onBackPress);
+        }, [canGoBack])
+    );
+
+    // 안드로이드의 소프트웨어적인 뒤로가기 설정
+    const backPress = (navState) => {
+        const { canGoBack } = navState;
+
+        if (canGoBack) {
+            navigation.setParams({
+                isCanBack: {
+                    title: "",
+                    onPress: () => webview.current.goBack(),
+                },
+            });
+        } else {
+            navigation.setParams({
+                isCanBack: null,
+            });
+        }
+    };
+
+    const onShouldStartLoadWithRequest = (event) => {
+        if (
+            event.url.startsWith('http://') ||
+            event.url.startsWith('https://') ||
+            event.url.startsWith('about:blank')
+        ) {
+            return true;
+        }
+        if (Platform.OS === 'android') {
+            const SendIntentAndroid = require('react-native-send-intent');
+            SendIntentAndroid.isAppInstalled(event.url)
+                .then(data => alert(data + '+' + event.url
+                ))
+            SendIntentAndroid.openApp(event.url)
+                .then(isOpened => {
+                    if (!isOpened) { alert('앱 실행이 실패했습니다'); }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+    
+            return false;
+    
+        } else {
+            Linking.openURL(event.url)
+                .catch(err => {
+                    alert('앱 실행이 실패했습니다. 설치가 되어있지 않은 경우 설치하기 버튼을 눌러주세요.');
+                });
+            return false;
+        }
+    };
+
+
     return (
         <WebView
             source={{
-                uri: url,
+                uri: url
             }}
-            originWhitelist={['http://', 'https://', 'intent://']}
+            originWhitelist={['http://', 'https://', 'intent://', '*']}
             onShouldStartLoadWithRequest={event => {
                 return onShouldStartLoadWithRequest(event);
             }}
-        /* ref={webview}
-        onNavigationStateChange={(navState) => {
-          SetCanGoBack(navState.canGoBack) + backPress(navState);
-        }}
-        injectedJavaScript={INJECTED_JAVASCRIPT}
-        onMessage={(event) => {}} */
+            startInLoadingState
+            ref={webview}
+            onNavigationStateChange={(navState) => {
+                SetCanGoBack(navState.canGoBack) + backPress(navState);
+            }}
+            injectedJavaScript={INJECTED_JAVASCRIPT}
+            onMessage={(event) => { }}
         />
     );
 };
--- view/content.js
+++ view/content.js
@@ -1,8 +1,41 @@
 import React from 'react';
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import AndroidPlatform from './androidWebView';
 
+const WebView = () => {
+    const url = "https://www.megahrd.co.kr/m/mobile/mobileTI/login.mbl";
+    return (
+        <View style={styles.root}>
+            <View style={styles.browser}>
+                <AndroidPlatform url={url} />
+            </View>
+        </View>
+    )
+}
+
+const Stack = createStackNavigator();
+
+const content = () => {
+    return (
+        <>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="WebView" component={WebView} options={{headerShown: false}}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+
+        </>
+    );
+};
+
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems:'center',
+        justifyContent: "center"
+    },
     browser: {
         flex: 1,
         flexDirection: "row",
@@ -10,22 +43,9 @@ const styles = StyleSheet.create({
     root: {
         flex: 1,
         backgroundColor: "#fff",
-        alignItems: "center",
         justifyContent: "center",
         display: "flex",
     },
 });
-
-
-const content = () => {
-    const url = "https://www.megahrd.co.kr/m/mobile/mobileTI/login.mbl";
-    return (
-        <View style={styles.root}>
-            <View style={styles.browser}>
-            <AndroidPlatform url={url} />
-            </View>
-        </View>
-    );
-};
 
 export default content;
