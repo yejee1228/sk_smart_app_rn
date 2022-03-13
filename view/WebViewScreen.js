4624fbd16175919100222b7e60d8de699ef2c3ef
yejee <yejee1228@gmail.com>
2022-03-13 14:45:48 +0900

    ios 웹뷰 설정

--- view/WebViewScreen.js
+++ view/WebViewScreen.js
@@ -2,17 +2,41 @@ import React from 'react';
 import { StyleSheet, View, Platform } from "react-native";
 import AndroidPlatform from './platform/Android';
 import IosPlatform from './platform/Ios';
+import { GestureRecognizer} from 'react-native-swipe-gestures';
 
 const WebViewScreen = () => {
     const url = "https://megac.megahrd.co.kr/m/mobile/mobileTI/login.mbl";
     //const url = "https://skshieldus.megahrd.co.kr/m/mobile/mobileTI/login.mbl";
+    const onSwipeDown = (gestureState) => {
+        this.setState({myText: 'You swiped down!'});
+      }
+    
+    const onSwipeLeft = (gestureState) => {
+        this.setState({myText: 'You swiped left!'});
+      }
+    
+    const onSwipeRight = (gestureState) => {
+        this.setState({myText: 'You swiped right!'});
+      }
     return (
+        <GestureRecognizer
+            onSwipeDown={(state) => this.onSwipeDown(state)}
+            onSwipeLeft={(state) => this.onSwipeLeft(state)}
+            onSwipeRight={(state) => this.onSwipeRight(state)}
+          config={{
+            velocityThreshold: 0.3,
+            directionalOffsetThreshold: 80,
+          }}
+          style={{
+            flex: 1,
+          }}>
             <View style={styles.browser}>
                 {Platform.OS === 'ios' ?
                     <IosPlatform url={url} /> :
                     <AndroidPlatform url={url} />
                 }
             </View>
+        </GestureRecognizer>
     )
 }
 
