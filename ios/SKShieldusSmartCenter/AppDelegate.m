796d2d7190ac2f6b1530135d0858ab7bfb4a8ac7
yejee1228 <yejee1228@megastudy.net>
2022-03-15 16:19:02 +0900

    rename

--- ios/SKShieldusSmartCenter/AppDelegate.m
+++ ios/SKShieldusSmartCenter/AppDelegate.m
@@ -36,7 +36,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"sk_smart_app_rn"
+                                                   moduleName:@"SKShieldusSmartCenter"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
