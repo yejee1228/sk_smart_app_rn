daa063321d3bcd7b07fff6c6bc063ae21fcf5b34
yejee1228 <yejee1228@megastudy.net>
2022-03-16 09:59:44 +0900

    아이폰 다크모드 미사용

--- ios/SKShieldusSmartCenter/AppDelegate.m
+++ ios/SKShieldusSmartCenter/AppDelegate.m
@@ -62,4 +62,8 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 }
 
+if (@available(iOS 13.0, *)) {
+  rootView.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+}
+
 @end
