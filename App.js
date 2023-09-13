912e8f69950970514e2e7c842eb5880caeac9d59
yejee1228 <yejee1228@gmail.com>
2023-09-13 17:02:32 +0900

    필수 업데이트 안내 / 파일다운로드 시 알림 및 파일 열기 기능 추가

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React, {useEffect} from 'react';
-import SplashScreen from 'react-native-splash-screen';
-import {View, StyleSheet} from 'react-native'
+import React, { useEffect } from 'react'
+import SplashScreen from 'react-native-splash-screen'
+import { View, StyleSheet, Alert, Linking } from 'react-native'
 import HomeScreen from './view/HomeScreen'
+import VersionCheck from 'react-native-version-check'
+import SendIntentAndroid from 'react-native-send-intent'
 
 const styles = StyleSheet.create({
   container: {
@@ -9,20 +11,54 @@ const styles = StyleSheet.create({
     backgroundColor: '#ccc',
     justifyContent: 'center',
   },
-});
+})
 
 const App = () => {
+  const AppVersionCheck = async () => {
+    //기기에 설치되 있는 버전
+    let CurrentVersion = VersionCheck.getCurrentVersion()
+    //앱의 최신버전
+    let LatestVersion = await VersionCheck.getLatestVersion()
+    console.log('version:', CurrentVersion, LatestVersion)
+    //기기에 설치되있는 버전과 앱에 올려져있는 최신버전을 비교
+    VersionCheck.needUpdate({
+      currentVersion: CurrentVersion,
+      latestVersion: LatestVersion,
+    }).then((res) => {
+      console.log(res)
+      if (res.isNeeded) {
+        Alert.alert('필수 업데이트 사항이 있습니다.', '', [
+          {
+            text: '스토어이동',
+            onPress: () => {
+              if (Platform.OS == 'android') {
+                SendIntentAndroid.openAppWithUri(
+                  'https://play.google.com/store/apps/details?id=com.sk_smart_app_rn&pcampaignid=web_share'
+                )
+              } else {
+                Linking.openURL(
+                  'https://itunes.apple.com/kr/app/sk-shieldus-smart-learning/id1614934713'
+                )
+              }
+            },
+          },
+        ])
+      }
+    })
+  }
+
   useEffect(() => {
     setTimeout(() => {
-      SplashScreen.hide();
-    }, 1500);
-  }, []);
+      SplashScreen.hide()
+      AppVersionCheck()
+    }, 1500)
+  }, [])
 
   return (
     <View style={styles.container}>
-      <HomeScreen/>
+      <HomeScreen />
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- view/platform/Android.js
+++ view/platform/Android.js
@@ -1,15 +1,23 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { BackHandler, KeyboardAvoidingView, Alert } from 'react-native'
 import { useFocusEffect } from '@react-navigation/native'
 import { WebView } from 'react-native-webview'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import GestureRecognizer from 'react-native-swipe-gestures'
 import RNFS from 'react-native-fs'
+import PushNotification from 'react-native-push-notification'
+import { PERMISSIONS, request, check, RESULTS } from 'react-native-permissions'
+import SendIntentAndroid from 'react-native-send-intent'
 
 const Android = ({ url }) => {
   const webview = useRef(null)
   const [canGoBack, setCanGoBack] = useState(false)
   const [start, setStart] = useState(false)
+
+  useEffect(() => {
+    // 앱 실행 시 알림권한 요청
+    checkNotificationsPermission()
+  }, [])
 
   // 하드웨어적인 뒤로가기 설정
   useFocusEffect(
@@ -36,11 +44,10 @@ const Android = ({ url }) => {
     ) {
       return true
     }
-    const SendIntentAndroid = require('react-native-send-intent')
     SendIntentAndroid.openAppWithUri(event.url)
       .then((isOpened) => {
         if (!isOpened) {
-          alert('앱 실행이 실패했습니다')
+          Alert.alert('앱 실행이 실패했습니다')
         }
       })
       .catch((err) => {
@@ -50,43 +57,107 @@ const Android = ({ url }) => {
     return false
   }
 
+  // 알림 초기설정
+  PushNotification.createChannel(
+    {
+      channelId: 'default-channel-id',
+      channelName: 'Default channel',
+      channelDescription: 'A default channel',
+    },
+    () => {}
+  )
+
+  PushNotification.configure({
+    onNotification: (notification) => {
+      SendIntentAndroid.openFileChooser(
+        {
+          subject: 'File subject', //optional,
+          fileUrl: notification.data.filePath,
+          type: '*/*',
+        },
+        'Open file with:'
+      )
+    },
+    requestPermissions: false,
+  })
+
+  // 권한 체크 및 요청 함수
+  const checkAndRequestPermission = async (permission) => {
+    try {
+      const status = await check(permission)
+
+      if (status === RESULTS.GRANTED) {
+        return true
+      } else if (status === RESULTS.DENIED) {
+        // 사용자가 권한을 거부한 경우 권한 요청
+        const requestStatus = await request(permission)
+        if (requestStatus === RESULTS.GRANTED) {
+          return true
+        } else {
+          return false
+        }
+      } else {
+        return false
+      }
+    } catch (error) {
+      return false
+    }
+  }
+
+  const checkNotificationsPermission = async () => {
+    const permission = PERMISSIONS.ANDROID.POST_NOTIFICATIONS
+    await checkAndRequestPermission(permission)
+  }
+
   //자동로그인, 다운로드
-  const handleOnMessage = async ({ nativeEvent }) => {
+  const handleOnMessage = ({ nativeEvent }) => {
     //웹뷰 정보 받음.
     let data = JSON.parse(nativeEvent.data)
     if (data.type === 'download') {
-      //다운로드
-      const url = data.url
-      const originalFileName = data.fileName
-      const downloadFolder = RNFS.DownloadDirectoryPath
-
-      try {
-        const uniqueFileName = await generateUniqueFileName(originalFileName, downloadFolder)
-        const destPath = `${RNFS.DownloadDirectoryPath}/${uniqueFileName}`
-        const options = {
-          fromUrl: url,
-          toFile: destPath,
-        }
-        const downloadResult = await RNFS.downloadFile(options)
-        downloadResult.promise
-          .then((result) => {
-            if (result.statusCode === 200) {
-              Alert.alert('다운로드 완료', `${uniqueFileName}가 다운로드 폴더에 저장되었습니다.`)
-            } else {
-              Alert.alert('다운로드 실패', '파일 다운로드가 실패했습니다.')
-            }
-          })
-          .catch((error) => {
-            console.error(error)
-            Alert.alert('오류', '다운로드에 오류가 발생했습니다. 고객센터에 문의해주세요.')
-          })
-      } catch (error) {
-        console.error(error)
-        Alert.alert('오류', '다운로드에 오류가 발생했습니다. 고객센터에 문의해주세요.')
-      }
+      //파일 다운로드
+      downloadFile(data)
     } else {
       //자동로그인
       AsyncStorage.setItem('logininfo', JSON.stringify(data))
+    }
+  }
+
+  //파일 다운로드
+  const downloadFile = async (data) => {
+    const url = data.url
+    const originalFileName = data.fileName
+    const downloadFolder = RNFS.DownloadDirectoryPath
+
+    try {
+      const uniqueFileName = await generateUniqueFileName(originalFileName, downloadFolder)
+      const destPath = `${downloadFolder}/${uniqueFileName}`
+      const options = {
+        fromUrl: url,
+        toFile: destPath,
+      }
+      const downloadResult = RNFS.downloadFile(options)
+      downloadResult.promise
+        .then(async (result) => {
+          if (result.statusCode === 200) {
+            PushNotification.localNotification({
+              channelId: 'default-channel-id',
+              title: '다운로드 완료',
+              message: uniqueFileName,
+              data: {
+                filePath: destPath,
+              },
+            })
+          } else {
+            Alert.alert('다운로드 실패', '파일 다운로드가 실패했습니다.')
+          }
+        })
+        .catch((error) => {
+          console.error(error)
+          Alert.alert('오류', '다운로드에 오류가 발생했습니다. 고객센터에 문의해주세요.')
+        })
+    } catch (error) {
+      console.error(error)
+      Alert.alert('오류', '다운로드에 오류가 발생했습니다. 고객센터에 문의해주세요.')
     }
   }
 
@@ -150,6 +221,7 @@ const Android = ({ url }) => {
           }}
           onMessage={handleOnMessage}
           onLoadStart={sendMessage}
+          androidHardwareAccelerationDisabled={true}
         />
       </KeyboardAvoidingView>
     </GestureRecognizer>
